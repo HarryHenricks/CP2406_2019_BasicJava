8cbf72bf8714313bde2d6217bb5f8bf561fa42e6
Harry <36616945+SinexOG@users.noreply.github.com>
2019-10-21 18:40:38 +1000

    Started programming exercises chapter 15

---
+++ CP2406_Prac1/src/week10/ProgrammingExercisesChapter15/JMovieFrame.java
@@ -0,0 +1,16 @@
+package week10.ProgrammingExercisesChapter15;
+
+import javax.swing.*;
+
+public class JMovieFrame {
+    // create a JFrame and set the layout to BorderLayout.
+    // In each region, place a JButton that displays the name of a
+    // classic movie that has the region name in its title.
+    // For example, the east button might indicate the movie East of Eden.
+    // When the user clicks the button, display the year of the movies release
+    // and the name of one of its stars (actors)
+
+
+    JFrame mainFrame = new JFrame();
+
+}
